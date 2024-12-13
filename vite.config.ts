c0bdb457d15667bd348186bee89eb28ccd20bfac
vinibispo <vini.bispo015@gmail.com>
2024-12-12 23:37:30 -0300

    Basic setup of PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true },
+      manifest: {
+        background_color: '#FFFFFF',
+        theme_color: '#6425FE',
+        name: 'Stock App Designli',
+        short_name: 'Stock App',
+        description: 'A simple stock app using the finnhub API',
+      }
+    })
+  ],
 })
