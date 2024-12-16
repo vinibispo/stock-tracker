f3f66ce23a8e405f930e28e0d7e33440a63fcb54
vinibispo <vini.bispo015@gmail.com>
2024-12-16 20:35:30 -0300

    Add react-hook-form and make fields required

--- src/components/LeftForm.tsx
+++ src/components/LeftForm.tsx
@@ -1,45 +1,61 @@
-import { FormEventHandler } from "react"
 import { useStock } from "../hooks/useStock"
 import { fetchQuote } from "../services/api"
 import { Autocomplete, Button, Card, CardContent, Container, TextField } from "@mui/material"
 import { Stock } from "../typing"
+import { Controller, useForm } from "react-hook-form"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
 
+const stockFormSchema = z.object({
+  symbol: z.string().nonempty(),
+  alertPrice: z.coerce.number().positive()
+})
+type StockFormInput = z.infer<typeof stockFormSchema>
 export default function LeftForm() {
-  const { stockOptions : options, onAddStock, stockSymbolInput, stocks } = useStock()
-  const handleAddStock: FormEventHandler<HTMLFormElement> = async(event) => {
-    event.preventDefault()
-    const form = event.currentTarget
-    const formData = new FormData(form)
-    const symbol = formData.get('symbol')
-    const alertPriceStr = formData.get('alertPrice')
-    if (!symbol) {
-      event.currentTarget.focus()
-      return
+  const { control, handleSubmit, reset, formState: { errors } } = useForm<StockFormInput>({
+    resolver: zodResolver(stockFormSchema),
+    defaultValues: {
+      symbol: '',
+      alertPrice: 0,
     }
-    if (!alertPriceStr) return
-    if (symbol instanceof File || alertPriceStr instanceof File) return
+  })
+  const { stockOptions: options, onAddStock, stockSymbolInput, stocks } = useStock()
+  const handleSubmitStockForm = async (data: StockFormInput) => {
+    const { symbol, alertPrice } = data
 
-    const alertPrice = Number(alertPriceStr)
     const currentStock = stocks.find(stock => stock.symbol == symbol)
     let stock: Stock
     if (!currentStock) {
-    
-    const quote = await fetchQuote(symbol)
-    stock  = {...quote, symbol, alertPrice }
-  } else {
-    stock = {...currentStock, alertPrice } 
-  }
+
+      const quote = await fetchQuote(symbol)
+      stock = { ...quote, symbol, alertPrice }
+    } else {
+      stock = { ...currentStock, alertPrice }
+    }
     onAddStock(stock)
-    if (form) form.reset()
+    reset()
   }
   return (
     <Container>
       <Card>
-        <CardContent sx={{ display: 'flex', flexDirection: 'column', gap: 2 }} component="form" onSubmit={handleAddStock}>
-          <Autocomplete renderInput={(params) => <TextField {...params} label="Select Stock" name="symbol" inputRef={stockSymbolInput} />}
-            options={options}
+        <CardContent sx={{ display: 'flex', flexDirection: 'column', gap: 2 }} component="form" onSubmit={handleSubmit(handleSubmitStockForm)}>
+          <Controller
+            name="symbol"
+            control={control}
+            render={({ field }) => (
+              <Autocomplete
+                renderInput={(params) => <TextField {...field} {...params} label="Select Stock" inputRef={stockSymbolInput} error={!!errors.symbol} helperText={errors.symbol?.message} />}
+                options={options}
+              />
+            )}
           />
-          <TextField label="Price Alert" name="alertPrice" />
+          <Controller
+            name="alertPrice"
+            control={control}
+            render={({ field }) => (
+              <TextField {...field} label="Price Alert" error={!!errors.alertPrice} helperText={errors.alertPrice?.message} type="number" />
+            )}
+          />
           <Button variant="contained" type="submit">Add Stock</Button>
         </CardContent>
       </Card>
